f26191b510743c01663e4ca15a74177f924d37b3
CapellariGui <capellarigui@hotmail.com>
2021-08-21 10:24:47 -0300

    Aula-01

---
+++ Scripts/01-rstudio.R
@@ -0,0 +1,23 @@
+##Configurando deretorio
+
+getwd()
+
+## Contribuidores
+contributors()
+
+print("Bom Sábado")
+
+## Criar um Gráfico
+plot(1:25)
+
+## Instalar Pacotes
+installed.packages()
+install.packages("randomForest")
+
+## Carregando Pacotes
+library(randomForest)
+
+## Descarregando pacotes
+detach(package:randomForest)
+
+
