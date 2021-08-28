e19dee7f64799b6f51be43c6826f83391570157d
CapellariGui <capellarigui@hotmail.com>
2021-08-28 09:14:01 -0300

    script 02

--- Scripts/01-rstudio.R
+++ Scripts/01-rstudio.R
@@ -33,3 +33,72 @@ library(sos)
 findFn("detach")
 
 print("Salvando Primeiro Arquivo")
+
+### AULA 2 ###
+
+# Operadores Básicos, Relacionais e Lógicos em R
+
+# Operadores Básicos 
+
+# Soma
+
+5 + 5
+
+# Subtração 
+
+7 - 3 
+
+# Multiplicação 
+5 * 3 
+
+# Divisão
+5 / 3 
+
+# Potência 
+
+3 ^ 2 
+3 ** 2
+
+# Módulo
+16 %% 3
+
+# Operadores relacionais
+x <- 7
+y <- 5 
+
+x + y
+X <- 3
+x
+
+# Operadores
+X > 7
+x < 8
+X <= 8
+x >= 8 
+X == 8 
+x != 8
+
+# Operadores Lógicos
+
+#  And
+(x==8) & (x==6)
+(x==7) & (x>=5)
+(x==8) & (x==7)
+
+# Or
+
+(x==8) |  (x>5)
+(x==8) | (x>=5)
+
+# Not
+
+x > 8
+print(!x > 8)
+
+
+
+
+
+
+
+
